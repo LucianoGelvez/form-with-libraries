15378982367f3cf61a329ff76e290ac4286def7f
LucianoGelvez <Lucianomagallanes55@gmail.com>
2023-10-29 11:28:56 -0300

    first

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '"http://lucianogelvez.github.io/Formulario-with-formik-yup-materialUI-reactRouterDom/"'
 })
